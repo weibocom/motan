97ecdfd36358072cf4d0bd012899f71ee481f3ff
Ray <zzll0603@126.com>
2016-06-08 15:44:18 +0800

    add demo start shell

---
+++ motan-demo/motan-demo-client/src/main/bin/stop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+PWD=`pwd`
+cd `dirname $0`
+cd ../lib
+LIB_DIR=`pwd`
+
+SERVER_NAME='com.weibo.motan.demo.client.DemoRpcClient'
+PIDS=`ps -ef | grep java | grep "$LIB_DIR" |grep $SERVER_NAME|awk '{print $2}'`
+if [ -z "$PIDS" ]; then
+    echo "stop fail! The $SERVER_NAME not start!"
+    exit 1
+fi
+
+for PID in $PIDS ; 
+do
+    kill $PID > /dev/null 2>&1
+done
+echo "stop success! pid:"$PIDS
+cd $PWD
