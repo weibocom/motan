4e2cc27fd7a65bf802ed069d0f91da5ed9884253
Ray <zzll0603@126.com>
2016-08-05 18:05:23 +0800

    add spring-boot demo

---
+++ motan-demo/motan-demo-client/src/main/java/com/weibo/motan/demo/client/SpringBootRpcClientDemo.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2009-2016 Weibo, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.weibo.motan.demo.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.weibo.api.motan.config.springsupport.AnnotationBean;
+import com.weibo.api.motan.config.springsupport.BasicRefererConfigBean;
+import com.weibo.api.motan.config.springsupport.ProtocolConfigBean;
+import com.weibo.api.motan.config.springsupport.RegistryConfigBean;
+
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class SpringBootRpcClientDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRpcClientDemo.class, args);
+    }
+
+
+
+    @Bean
+    public AnnotationBean motanAnnotationBean() {
+        AnnotationBean motanAnnotationBean = new AnnotationBean();
+        motanAnnotationBean.setPackage("com.weibo.motan.demo.client");
+        return motanAnnotationBean;
+    }
+
+    @Bean(name = "demoMotan")
+    public ProtocolConfigBean protocolConfig1() {
+        ProtocolConfigBean config = new ProtocolConfigBean();
+        config.setDefault(true);
+        config.setName("motan");
+        config.setMaxContentLength(1048576);
+        return config;
+    }
+
+    @Bean(name = "registry")
+    public RegistryConfigBean registryConfig() {
+        RegistryConfigBean config = new RegistryConfigBean();
+        config.setRegProtocol("local");
+        return config;
+    }
+
+    @Bean(name = "motantestClientBasicConfig")
+    public BasicRefererConfigBean baseRefererConfig() {
+        BasicRefererConfigBean config = new BasicRefererConfigBean();
+        config.setProtocol("demoMotan");
+        config.setGroup("motan-demo-rpc");
+        config.setModule("motan-demo-rpc");
+        config.setApplication("myMotanDemo");
+        config.setRegistry("registry");
+        config.setCheck(false);
+        config.setAccessLog(true);
+        config.setRetries(2);
+        config.setThrowException(true);
+        return config;
+    }
+
+
+}
---
+++ motan-demo/motan-demo-server/src/main/java/com/weibo/motan/demo/server/SpringBootRpcServerDemo.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2009-2016 Weibo, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.weibo.motan.demo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.weibo.api.motan.common.MotanConstants;
+import com.weibo.api.motan.config.springsupport.AnnotationBean;
+import com.weibo.api.motan.config.springsupport.BasicServiceConfigBean;
+import com.weibo.api.motan.config.springsupport.ProtocolConfigBean;
+import com.weibo.api.motan.config.springsupport.RegistryConfigBean;
+import com.weibo.api.motan.util.MotanSwitcherUtil;
+
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class SpringBootRpcServerDemo {
+
+    public static void main(String[] args) {
+        System.setProperty("server.port", "8081");
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootRpcServerDemo.class, args);
+        MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
+        System.out.println("server start...");
+    }
+
+    @Bean
+    public AnnotationBean motanAnnotationBean() {
+        AnnotationBean motanAnnotationBean = new AnnotationBean();
+        motanAnnotationBean.setPackage("com.weibo.motan.demo.server");
+        return motanAnnotationBean;
+    }
+
+    @Bean(name = "demoMotan")
+    public ProtocolConfigBean protocolConfig1() {
+        ProtocolConfigBean config = new ProtocolConfigBean();
+        config.setDefault(true);
+        config.setName("motan");
+        config.setMaxContentLength(1048576);
+        return config;
+    }
+
+    @Bean(name = "registryConfig1")
+    public RegistryConfigBean registryConfig() {
+        RegistryConfigBean config = new RegistryConfigBean();
+        config.setRegProtocol("local");
+        return config;
+    }
+
+    @Bean
+    public BasicServiceConfigBean baseServiceConfig() {
+        BasicServiceConfigBean config = new BasicServiceConfigBean();
+        config.setExport("demoMotan:8002");
+        config.setGroup("testgroup");
+        config.setAccessLog(false);
+        config.setShareChannel(true);
+        config.setModule("motan-demo-rpc");
+        config.setApplication("myMotanDemo");
+        config.setRegistry("registryConfig1");
+        return config;
+    }
+
+}
