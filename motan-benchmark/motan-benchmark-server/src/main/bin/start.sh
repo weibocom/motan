936a63e9ba3ffeea7de090cfb1bc12a094a93aa5
张羽 <zhangyu21@staff.weibo.com>
2016-09-12 12:35:33 +0800

    conf: benchmark启动脚本

---
+++ motan-benchmark/motan-benchmark-server/src/main/bin/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+DIR=`pwd`
+cd `dirname $0`
+cd ../lib
+LIB_DIR=`pwd`
+
+SERVER_NAME='com.weibo.motan.benchmark.MotanBenchmarkServer'
+
+PIDS=`ps -ef | grep java | grep "$LIB_DIR" | grep ${SERVER_NAME} | awk '{print $2}'`
+if [ -n "$PIDS" ]; then
+    echo "start failed, the $SERVER_NAME already started!"
+    exit 1
+fi
+
+LIB_JARS=`ls ${LIB_DIR} | grep .jar | awk '{print "'${LIB_DIR}'/"$0}' | tr "\n" ":"`
+cd ..
+nohup java -classpath ${LIB_JARS} ${SERVER_NAME} nohup.server.out 2>&1 &
+
+echo "start "${SERVER_NAME}" success!"
+cd ${DIR}
