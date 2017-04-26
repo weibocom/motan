5cdb95357ce82610711315d57dbc9a2cdea444dc
湛然 <shixi_zhanran@staff.weibo.com>
2017-04-26 15:39:13 +0800

    update to newest motan master

---
+++ motan-core/src/main/java/com/weibo/api/motan/shutdown/Closable.java
@@ -0,0 +1,8 @@
+package com.weibo.api.motan.shutdown;
+
+/**
+ * Created by voyager on 2017/4/26.
+ */
+public interface Closable {
+    void close();
+}
---
+++ motan-core/src/main/java/com/weibo/api/motan/shutdown/ShutDownHook.java
@@ -0,0 +1,9 @@
+package com.weibo.api.motan.shutdown;
+
+/**
+ * Created by voyager on 2017/4/26.
+ */
+public class ShutDownHook extends Thread{
+
+
+}
