bf714f1728be305ee30e80db8d9cc9cbcdcec83d
konglingzhen <Dy5861418>
2018-01-02 13:25:45 +0800

    去掉多余的字段

--- motan-spring-boot-starter/src/main/java/com/weibo/api/motan/spring/boot/autoconfigure/MotanAutoConfiguration.java
+++ motan-spring-boot-starter/src/main/java/com/weibo/api/motan/spring/boot/autoconfigure/MotanAutoConfiguration.java
@@ -4,12 +4,9 @@ import com.weibo.api.motan.config.springsupport.*;
 import com.weibo.api.motan.spring.boot.autoconfigure.actuator.ActuatorAutoConfiguration;
 import com.weibo.api.motan.spring.boot.autoconfigure.properties.MotanBeanNames;
 import com.weibo.api.motan.spring.boot.autoconfigure.properties.MotanProperties;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,8 +20,6 @@ import org.springframework.util.Assert;
 @ConditionalOnClass(com.weibo.api.motan.rpc.Exporter.class)
 @Import(ActuatorAutoConfiguration.class)
 public class MotanAutoConfiguration {
-
-    private static final Logger log = LoggerFactory.getLogger(MotanAutoConfiguration.class);
 
     @Bean(name = MotanBeanNames.ANNOTATION_BEAN_NAME)
     @ConditionalOnMissingBean
