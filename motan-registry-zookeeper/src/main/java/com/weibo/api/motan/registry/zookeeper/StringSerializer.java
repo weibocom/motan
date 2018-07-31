d21c36ace4395a17624d12ffa3088f896a366fb2
zhachen <zhachen@staff.weibo.com>
2018-07-31 14:04:38 +0800

    test

--- motan-registry-zookeeper/src/main/java/com/weibo/api/motan/registry/zookeeper/StringSerializer.java
+++ motan-registry-zookeeper/src/main/java/com/weibo/api/motan/registry/zookeeper/StringSerializer.java
@@ -1,32 +1,31 @@
 package com.weibo.api.motan.registry.zookeeper;
 
+import com.weibo.api.motan.util.ByteUtil;
 import org.I0Itec.zkclient.exception.ZkMarshallingError;
 import org.I0Itec.zkclient.serialize.SerializableSerializer;
 
 import java.io.ObjectStreamConstants;
+import java.io.UnsupportedEncodingException;
 
 public class StringSerializer extends SerializableSerializer {
     @Override
     public Object deserialize(byte[] bytes) throws ZkMarshallingError {
-        if (getTag(bytes) == ObjectStreamConstants.STREAM_MAGIC) {
-            return super.deserialize(bytes);
+        try {
+            if (ByteUtil.bytes2short(bytes, 0) == ObjectStreamConstants.STREAM_MAGIC) {
+                return super.deserialize(bytes);
+            }
+            return new String(bytes, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new ZkMarshallingError(e);
         }
-        return new String(bytes);
     }
 
     @Override
     public byte[] serialize(Object obj) throws ZkMarshallingError {
-        return obj.toString().getBytes();
-    }
-
-    private short getTag(byte[] bytes) {
-        short s = 0;
-        if (bytes.length > 2) {
-            for (int i = 0; i < 2; i++) {
-                s <<= 8;
-                s |= (bytes[i] & 0x00ff);
-            }
+        try {
+            return obj.toString().getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new ZkMarshallingError(e);
         }
-        return s;
     }
 }
